1f72685579d2c444f2192e23fba9b1ad7b902430
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 02:18:42 +0900

    feat: signin page markup 작업

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,17 +1,38 @@
 <template>
-  <div>
-    <h1>SignIn</h1>
+  <div class="signin">
+    <h1>signin</h1>
+    <form class="signin__form" @submit="handleSubmit">
+      <label for="id">
+        <input
+          type="text"
+          v-model="id"
+          name="id"
+          placeholder="ID를 입력해주세요"
+        />
+      </label>
+      <label for="password">
+        <input
+          type="text"
+          v-model="password"
+          name="password"
+          placeholder="PASSWORD를 입력해주세요"
+        />
+      </label>
+      <button>로그인</button>
+      <router-link to="/reset-password">비밀번호 재설정</router-link>
+    </form>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
+const id = ref<string | null>(null)
+const password = ref<string | null>(null)
 
-export default defineComponent({
-  setup() {
-    return {}
-  }
-})
+function handleSubmit(e: Event) {
+  e.preventDefault()
+  console.log(id.value, password.value)
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped></style>
