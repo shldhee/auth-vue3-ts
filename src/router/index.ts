d77799b60d289d9fb95fc93c6b11ce7a389aab3d
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-07 17:54:10 +0900

    feat: routing 작업
    - signin, reset-password, user  페이지 생성

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,23 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/signin',
+    name: 'Signin',
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+      import(/* webpackChunkName: "signin" */ '../views/Signin.vue')
+  },
+  {
+    path: '/reset-password',
+    name: 'ResetPassword',
+    component: () =>
+      import(
+        /* webpackChunkName: "reset-password" */ '../views/ResetPassword.vue'
+      )
+  },
+  {
+    path: '/user',
+    name: 'User',
+    component: () => import(/* webpackChunkName: "user" */ '../views/User.vue')
   }
 ]
 
---
+++ src/views/Signin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>SignIn</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
