8b8f3477e079874f2deb6e97c2ad935cab6e8844
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-07 17:54:10 +0900

    feat: routing 작업
    - signin, reset-password, user  페이지 생성

---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>User</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
