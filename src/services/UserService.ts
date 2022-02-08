fb958fe50f959896631d2d115572f0c103c57a4d
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 21:40:14 +0900

    feat: 로그아웃 기능 작업

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,5 @@
 import { LoginRequestType, UserInfoType } from '@/types'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 const BASE_URL = 'https://ably-frontend-assignment-server.vercel.app/api'
 
@@ -14,9 +14,24 @@ export default class UserService {
   public static async getUserInfo(token: string): Promise<UserInfoType> {
     const response = await axios.get(`${BASE_URL}/user`, {
       headers: {
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`
       }
     })
+
+    return response.data
+  }
+  public static async logout(token: string): Promise<AxiosResponse> {
+    const response = await axios.post(
+      `${BASE_URL}/logout`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
 
     return response.data
   }
--- src/views/User.vue
+++ src/views/User.vue
@@ -5,7 +5,8 @@
     :email="userInfo.email"
     :profile-image="userInfo.profileImage"
   />
-  <BasicButton>로그아웃</BasicButton>
+  <BasicButton @click="logout">로그아웃</BasicButton>
+  <p>{{ errorMessage }}</p>
 </template>
 
 <script setup lang="ts">
@@ -13,9 +14,23 @@ import { useAuth } from '@/store/auth'
 import UserCard from '@/components/UserCard.vue'
 import BasicButton from '@/components/BasicButton.vue'
 import { storeToRefs } from 'pinia'
+import router from '@/router'
+import { ref } from 'vue'
+import { AxiosResponse } from 'axios'
 
 const auth = useAuth()
 const { userInfo } = storeToRefs(auth)
+const errorMessage = ref('')
+
+async function logout() {
+  const result = await auth.logout()
+  if (result === true) {
+    router.push('/signin')
+  } else {
+    const response = result as AxiosResponse
+    errorMessage.value = response.data.error.message
+  }
+}
 </script>
 
 <style scoped></style>
