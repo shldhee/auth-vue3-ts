ff2d9641f1092ae26aa7012916320b1e6d936845
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 17:16:13 +0900

    feat: 공통 컴포넌트 생성(BasicButton, BasicInput)
    - BasicInput은 update:model 사용해서 양방향 바인딩 처리

---
+++ src/components/BasicButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button :type="type" class="button">
+    <slot>default</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ButtonHTMLAttributes } from 'vue'
+
+interface Props {
+  type?: ButtonHTMLAttributes['type']
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'button'
+})
+</script>
+
+<style scoped lang="scss">
+.button {
+  color: #ffffff;
+  background-color: #ff5160;
+  border: 0;
+  border-radius: 4px;
+  padding: 8px 16px;
+  font-size: 14px;
+}
+</style>
