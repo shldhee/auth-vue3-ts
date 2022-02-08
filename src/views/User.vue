4cf3a01f5f66b8afe92a9f37def2abf42eef9063
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 21:04:40 +0900

    fix: user 페이지 통신 router guard로 변경
    - router 접근전에 API 통신으로 변경
    - onMounted보다 더 빠르게 통신하고 처리함

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,29 +1,17 @@
 <template>
-  <div>
-    <h1>User</h1>
-    <UserCard v-bind="userInfo" />
-    <BasicButton>로그아웃</BasicButton>
-    <p>{{ errorMessage }}</p>
-  </div>
+  <h1>User</h1>
+  <UserCard v-bind="userInfo" />
+  <BasicButton>로그아웃</BasicButton>
 </template>
 
 <script setup lang="ts">
 import { useAuth } from '@/store/auth'
-import { onMounted, ref } from 'vue'
 import UserCard from '@/components/UserCard.vue'
 import BasicButton from '@/components/BasicButton.vue'
 import { storeToRefs } from 'pinia'
 
 const auth = useAuth()
 const { userInfo } = storeToRefs(auth)
-const errorMessage = ref<string | null>(null)
-
-onMounted(async () => {
-  const result = await auth.fetchUserInfo()
-  if (typeof result !== 'boolean') {
-    errorMessage.value = result.data.error.message
-  }
-})
 </script>
 
 <style scoped></style>
