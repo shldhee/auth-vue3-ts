83a064e674cef38b817e4ead8935ad3b79fe818e
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 23:17:02 +0900

    feat: Counter 컴포넌트 생성(MM:SS)
    - MM:SS 시간 나타내주는 컴포넌트
    - convertTimeFormat Util 함수 생성

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>{{ convertTimeFormat(remainTimeData) }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, toRefs } from 'vue'
+import convertTimeFormat from '@/utils/convertTimeFormat'
+
+interface Props {
+  remainTime: number
+}
+const props = defineProps<Props>()
+const { remainTime } = toRefs(props)
+const interval = 1000 // 1 second
+const remainTimeData = ref(remainTime.value / interval)
+
+onMounted(() => {
+  const repeat = () => {
+    remainTimeData.value--
+    if (remainTimeData.value > 0) {
+      setTimeout(repeat, 1000)
+    }
+  }
+  repeat()
+})
+</script>
+ㅐ
+
+<style scoped></style>
