f134bc7c93e4d5d72e68c78ef767e818444b4ee5
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 17:59:00 +0900

    fix: error 로직 수정
    - service api 에서 에러 처리하지 않고 스토어에서 에러처리
    - 코드도 간단해 지고 store에서 로직관리로 구조 변경

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,22 +1,14 @@
 import { LoginRequestType } from '@/types'
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 
 const BASE_URL = 'https://ably-frontend-assignment-server.vercel.app/api'
 
 export default class UserService {
-  public static async login(
-    requestData: LoginRequestType
-  ): Promise<AxiosResponse> {
-    let response
-    try {
-      response = await axios.post(`${BASE_URL}/login`, requestData, {
-        headers: { 'Content-Type': 'application/json' }
-      })
-      console.log('response : ', response)
-      return response
-    } catch (error: any) {
-      console.error(error?.response)
-      return error.response
-    }
+  public static async login(requestData: LoginRequestType): Promise<string> {
+    const response = await axios.post(`${BASE_URL}/login`, requestData, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+
+    return response.data.accessToken
   }
 }
