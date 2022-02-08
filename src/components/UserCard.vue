d25c4351606694cebe6acc2cd76be29c7d5bcf88
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-08 20:12:16 +0900

    feat: 회원정보 페이지 카드 작업
    - UserCard 컴포넌트 생성
    - 회원정보 API 연동

---
+++ src/components/UserCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <img :src="profileImage" :alt="`thumbnail ${name}`" />
+    <p>{{ name }}</p>
+    <p>{{ email }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+interface Props {
+  name: string
+  email: string
+  profileImage: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  name: 'default name',
+  email: 'default email',
+  profileImage: 'https://loremflickr.com/400/400'
+})
+
+const { name, email, profileImage } = toRefs(props)
+</script>
+
+<style scoped></style>
