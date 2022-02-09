188a31cc2c661414665ba842d9d1220c3407ae7a
Duckhee Lee <shldhee@yahoo.co.kr>
2022-02-09 11:55:33 +0900

    fix: type 회원정보페이지 접근 문구 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <h1 class="home__title">Home</h1>
     <p class="home__paragraph">위에 메뉴를 클릭해주세요</p>
+    <p class="home__paragraph">
+      로그인 되지 않은 상태에서는 회원정보 페이지 접근 불가
+    </p>
   </div>
 </template>
 
@@ -11,8 +14,6 @@
 .home {
   &__title {
     margin-bottom: 10px;
-  }
-  &__paragraph {
   }
 }
 </style>
